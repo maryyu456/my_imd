00969799662541b97bf71f53527377988f038594
MaryYu <mary.yu456@gmail.com>
2018-03-28 13:07:49 +0000

    update gemfile v1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MyImd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.secret_key_base = 'blipblapblup'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
